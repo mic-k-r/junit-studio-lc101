b9a42c10b025279030093bfef1c370744dd413cc
Michal Richter <michalrichter@Michals-Air.attlocal.net>
2021-02-01 20:32:38 -0600

    finished studio

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,6 +1,8 @@
 package main;
 
 
+import java.util.ArrayList;
+
 public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
@@ -22,14 +24,39 @@ public class BalancedBrackets {
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
+
+//        I overcomplicated way too much here:
+//        ArrayList<Character> brackets = new ArrayList<>();
+//        for (char ch : str.toCharArray()) {
+//            if (ch == '[') {
+//                brackets.add(ch);
+//            } else if (ch == ']') {
+//                brackets.add(ch);
+//            }
+//        }
+//        if (brackets.contains('[')) {
+//            if (brackets.indexOf('[') % 2 == 0 ) {
+//                brackets.remove('[');
+//            }
+//        } else if (brackets.contains(']')) {
+//            if (brackets.indexOf(']') % 2 != 0) {
+//                brackets.remove(']');
+//            }
+//        }
+//        return brackets.isEmpty();
+
+        if (str == null) {
+            throw new IllegalArgumentException("can't pass null");
+        }
+
         int brackets = 0;
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
                 brackets++;
             } else if (ch == ']') {
+                if (brackets < 1) return false;
                 brackets--;
             }
-        }
-        return brackets == 0;
+        } return brackets == 0;
     }
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,46 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
-    @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullThrowsIllegalArgumentException() {
+        BalancedBrackets.hasBalancedBrackets(null);
+        fail("This shouldn't happen because there should be an error");
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void unmatchedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
+
+    @Test
+    public void oneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void wordInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[hello]"));
+    }
+
+    @Test
+    public void phraseInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[hello, world!]"));
+    }
+
+    @Test
+    public void spaceInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[ ]"));
+    }
+
+    @Test
+    public void outOfOrderBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 
 }
